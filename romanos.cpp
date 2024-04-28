56823a07caf9e13ec03c2e32c21a807d14458404
emanuelob <oliveirabar.emanuel@gmail.com>
2024-04-28 17:01:25 -0300

    :white_check_mark: primeiro teste: passando em todos os testes

--- romanos.cpp
+++ romanos.cpp
@@ -1,6 +1,12 @@
 #include "romanos.hpp"
-int romanos_para_decimal(char const * num_romano)
-{
-  return 0; 
+
+int romanos_para_decimal(char const * num_romano) {
+  if (num_romano[0] == 'I') return 1;
+  if (num_romano[0] == 'V') return 5;
+  if (num_romano[0] == 'X') return 10;
+  if (num_romano[0] == 'L') return 50;
+  if (num_romano[0] == 'C') return 100;
+  if (num_romano[0] == 'D') return 500;
+  if (num_romano[0] == 'M') return 1000;
 }
  
