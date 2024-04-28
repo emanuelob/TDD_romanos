718f54c4f4ea8c52560879400e9ec626f1a9a656
emanuelob <oliveirabar.emanuel@gmail.com>
2024-04-28 16:58:42 -0300

    :white_check_mark: primeiro teste: algarismos únicos

--- testa_romanos.cpp
+++ testa_romanos.cpp
@@ -2,23 +2,15 @@
 #include "catch.hpp"
 #include "romanos.hpp"
 
-
-TEST_CASE( "Numeros romanos - algarismos únicos", "[romanos]" ) {
+TEST_CASE( "Numeros romanos - algarismos Ãºnicos", "[romanos]" ) {
     REQUIRE( romanos_para_decimal("I") == 1 );
-    
     REQUIRE( romanos_para_decimal("V") == 5 );
-
-    
+    REQUIRE( romanos_para_decimal("X") == 10 );
+    REQUIRE( romanos_para_decimal("L") == 50 );
+    REQUIRE( romanos_para_decimal("C") == 100 );
+    REQUIRE( romanos_para_decimal("D") == 500 );
+    REQUIRE( romanos_para_decimal("M") == 1000 );
 }
 
 
-
-
-TEST_CASE( "Numeros romanos - algarismos inválidos", "[romanos]" ) {
-    REQUIRE( romanos_para_decimal("G") == -1 );
-    
-    REQUIRE( romanos_para_decimal("i") == -1 );
-
-    
-}
  
