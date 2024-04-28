397bb2d769061265a535c355cc9e7a76d7615175
emanuelob <oliveirabar.emanuel@gmail.com>
2024-04-28 19:08:00 -0300

    :white_check_mark: terceiro teste: algarismos repetidos e subtraídos

--- testa_romanos.cpp
+++ testa_romanos.cpp
@@ -12,12 +12,28 @@ TEST_CASE("Numeros romanos - algarismos únicos", "[romanos]") {
     REQUIRE(romanos_para_decimal("M") == 1000);
 }
 
-TEST_CASE("Numeros romanos - algarismos duplicados", "[romanos]") {
-    REQUIRE(romanos_para_decimal("II") == 2);
+TEST_CASE("Numeros romanos - algarismos subtraidos", "[romanos]") {
     REQUIRE(romanos_para_decimal("IV") == 4);
     REQUIRE(romanos_para_decimal("IX") == 9);
     REQUIRE(romanos_para_decimal("XL") == 40);
     REQUIRE(romanos_para_decimal("XC") == 90);
     REQUIRE(romanos_para_decimal("CD") == 400);
     REQUIRE(romanos_para_decimal("CM") == 900);
+};
+
+TEST_CASE("Numeros romanos - algarismos repetidos", "[romanos]") {
+    REQUIRE(romanos_para_decimal("II") == 2);
+    REQUIRE(romanos_para_decimal("III") == 3);
+    REQUIRE(romanos_para_decimal("VII") == 7);
+    REQUIRE(romanos_para_decimal("VIII") == 8);
+    REQUIRE(romanos_para_decimal("XX") == 20);
+    REQUIRE(romanos_para_decimal("XXX") == 30);
+    REQUIRE(romanos_para_decimal("LXX") == 70);
+    REQUIRE(romanos_para_decimal("LXXX") == 80);
+    REQUIRE(romanos_para_decimal("CC") == 200);
+    REQUIRE(romanos_para_decimal("CCC") == 300);
+    REQUIRE(romanos_para_decimal("DCC") == 700);
+    REQUIRE(romanos_para_decimal("DCCC") == 800);
+    REQUIRE(romanos_para_decimal("MM") == 2000);
+    REQUIRE(romanos_para_decimal("MMM") == 3000);
 };
